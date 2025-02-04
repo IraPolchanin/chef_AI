3dc3018ddf560476281146d666a04dd0f26f1d30
Iryna Polchaninova <ira.polchanin@gmail.com>
2025-02-04 21:28:09 +0200

    Remove Hugging Face API key from history

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/chef_AI/",
+  define: {
+    "process.env.HUGGINGFACE_API_KEY": JSON.stringify(process.env.HUGGINGFACE_API_KEY)
+  },
   esbuild: {
     loader: 'tsx', // Дозволяє обробляти файли TypeScript },
   },
